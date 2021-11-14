e6d8354c1287185008ffc46c481ec8be54a4b58e
Benyaar <ghostmatt36@gmail.com>
2021-11-15 00:24:39 +0200

    Homework

--- js/main.js
+++ js/main.js
@@ -1,82 +1,83 @@
-// const contactList = [
-//     {
-//       id: 1,
-//       first_name: 'Vasya',
-//       last_name: 'Petrov',
-//       nick_name: '',
-//       phone: 093945394,
-//       group: [
-//         {
-//           name: 'friends',
-//           id: 2
-//         }
-//       ],
-//       actions: null
-//     },
-//     {
-//       id: 2,
-//       first_name: 'Petya',
-//       last_name: '',
-//       nick_name: 'Sizo',
-//       phone: 09722122,
-//       group: [
-//         {
-//           name: 'best_friends',
-//           id: 1
-//         }
-//       ],
-//       actions: [
-//         {
-//           name: 'missed call',
-//           value: 3,
-//           id: 1
-//         }
-//       ]
-//     },
-//     {
-//       id: 2,
-//       first_name: 'Rustem',
-//       last_name: '',
-//       nick_name: 'Box',
-//       phone: 09700000,
-//       group: [
-//         {
-//           name: 'other',
-//           id: 3
-//         }
-//       ],
-//       actions: [
-//         {
-//           name: 'missed call',
-//           value: 13,
-//           id: 1
-//         }
-//       ]
-//     }
-// ];
-// // console.log(contactList);
-
-// contactList.map(function(contact) {
-
-//     let groupName = contact.groups !== null ? contact.group.map(group => group.name) : '';
-
-//     console.log(
-//         `Имя: ${contact.first_name} \nФамилия: ${contact.last_name} \nТелефон: 0${contact.phone} \nГруппа ${groupName}`)  
-// });
 
 
-// let buttonElement = document.getElementById("btn");
+document.querySelector('.btn').onclick = myClick;
+function myClick() {
+     let a = document.querySelector('.name-1').value;
+     document.querySelector('.name').innerHTML = a;
+     let b = document.querySelector('.mail-1').value;
+     document.querySelector('.mail').innerHTML = b;
+     let c = document.querySelector('.message-1').value;
+     document.querySelector('.message').innerHTML = c;
+}    
+document.querySelector('.clear').onclick = myClear;
+function myClear() {
+     document.querySelector('.name').innerHTML = "";
+     document.querySelector('.mail').innerHTML = "";
+     document.querySelector('.message').innerHTML = "";
+} 
 
-// buttonElement.addEventListener('click', function (event) {
-//   console.log();
-// });
+let ArrayOfImages = ['../img/1.jpg', '../img/2.png', '../img/3.png'];
+ArrayOfImages.forEach(function(image) {
+  let img = document.createElement('img');
+  img.src = image;
+  img.height = "100";
+  img.width = "100";
+  document.getElementById("imageContainer").appendChild(img)
+});
 
-function getValue() {
-      let select = document.getElementById("select_");
-      let value = select.value;
-      console.log(value);
+document.querySelector('.test').onchange = myStyle;
+function myStyle() {
+     let sel = document.querySelector('.test').value;
+
+     switch (sel) {
+          case "red": 
+          document.querySelector(".divTest").style.backgroundColor = "red";
+          break;
+          case "blue": 
+          document.querySelector(".divTest").style.backgroundColor = "blue";
+          break;
+          case "green": 
+          document.querySelector(".divTest").style.backgroundColor = "green";
+          break;
+          case "yellow": 
+          document.querySelector(".divTest").style.backgroundColor = "yellow";
+          break;
+
+     }
+
+    //  if (sel ==="red"){
+    //       document.querySelector(".divTest").style.backgroundColor = "red";
+    //  }else if (sel ==="blue"){
+    //       document.querySelector(".divTest").style.backgroundColor = "blue";
+    //  }else if (sel ==="green"){
+    //       document.querySelector(".divTest").style.backgroundColor = "green";
+    //  }else if (sel ==="yellow"){
+    //       document.querySelector(".divTest").style.backgroundColor = "yellow";
+    //  }
 }
 
-// function getValue(value) {
-//   console.log(value);
-//   }
+let elem = document.querySelector('.elem')
+
+function createTable(tbl, rows, cols) {
+    for (let i = 0; i < rows; i++){
+        let tr = document.createElement('tr')
+    
+        for (let j = 0; j < cols; j++){
+            let td = document.createElement('td')
+            tr.appendChild(td)
+        }
+        elem.appendChild(tr)
+    }
+}
+
+document.querySelector('.send').onclick = mySend;
+function mySend() {
+     let row1 = document.querySelector('.row').value;
+     let cols1 = document.querySelector('.column').value;
+     createTable(elem, row1, cols1)
+}
+
+document.querySelector('.clear2').onclick = myClear2;
+function myClear2() {
+     document.querySelector('.elem').innerHTML = "";
+}
